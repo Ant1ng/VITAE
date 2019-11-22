3a018779266b1df2ee29bbdb95e0f03d428c6fcd
Anthony Ling <ant1ng@berkeley.edu>
2019-11-21 21:24:12 -0800

    Added stuff for the faq

--- app/src/main/java/com/example/vitae/Display.java
+++ app/src/main/java/com/example/vitae/Display.java
@@ -2,13 +2,29 @@ package com.example.vitae;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Display extends AppCompatActivity {
+
+    private ArrayList<String> text;
+    private LinearLayout faq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +32,49 @@ public class Display extends AppCompatActivity {
         setContentView(R.layout.activity_display);
         YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
         getLifecycle().addObserver(youTubePlayerView);
+
+        text = new ArrayList<>();
+        faq = findViewById(R.id.FQ);
+        initFAQ();
+        /*String filename = "myfile";
+        String fileContents = "Hello world!";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(fileContents.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
     }
+
+    String filename = "myfile";
+
+    protected void initFAQ() {
+        try {
+            FileInputStream fis = openFileInput(filename);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            text.add(reader.readLine());
+        }
+        catch (FileNotFoundException e) {
+            text.add("Rip");
+        }
+        catch (IOException e) {
+            text.add("Ripperino");
+        }
+        for (String i : text) {
+            writeQuestion(i);
+        }
+    }
+
+    private void writeQuestion(String question) {
+        TextView textView1 = new TextView(this);
+        textView1.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT));
+        textView1.setText(question);
+        textView1.setBackgroundColor(0xff66ff66); // hex color 0xAARRGGBB
+        textView1.setPadding(20, 20, 20, 20);// in pixels (left, top, right, bottom)
+        faq.addView(textView1);
+    } 
 }
