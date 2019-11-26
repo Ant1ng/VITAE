03896082530ab4c2d7e1661d1f94d9d1c6b3290d
sang-bae <sangbae@berkeley.edu>
2019-11-26 13:00:49 -0800

    keyboard hide when search

--- app/src/main/java/com/example/vitae/Search.java
+++ app/src/main/java/com/example/vitae/Search.java
@@ -3,6 +3,7 @@ package com.example.vitae;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -53,6 +55,10 @@ public class Search extends AppCompatActivity {
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
                         (keyCode == KeyEvent.KEYCODE_ENTER)) {
                     // Perform action on key press
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+
+
                     String text = searchBar.getText().toString();
                     text = text.replace(" ", "+");
                     try {
